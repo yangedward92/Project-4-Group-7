9e8a99d8e1b91a0f7760db776e693f028c8c0714
vhrico <vhrinco@gmail.com>
2025-04-09 18:23:14 -0400

    updated app

--- Diabetes App/model_helper.py
+++ Diabetes App/model_helper.py
@@ -25,25 +25,3 @@ class ModelHelper():
         has_diabetes = preds[0][1]
 
         return(has_diabetes)
-
-
-if __name__ == "__main__":
-    # Create an instance of ModelHelper
-    model_helper = ModelHelper()
-
-    # Provide sample inputs
-    Pregnancies = 2
-    Glucose = 120
-    BloodPressure = 70
-    SkinThickness = 20
-    Insulin = 85
-    BMI = 25.5
-    DiabetesPedigreeFunction = 0.5
-    Age = 30
-
-    # Call the make_predictions method
-    try:
-        result = model_helper.make_predictions(Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age)
-        print(f"Predicted probability of diabetes: {result}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
